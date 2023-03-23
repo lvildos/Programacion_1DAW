3b9024ee9ee382d5b896f235982cf6b74dd89fb3
lvildos <lvildos588@g.educaand.es>
2023-03-23 21:41:29 +0100

    Tareas

---
+++ EjerciciosLibroJava/Tema9.2/Ej1Cap9_2/PruebaGato.java
@@ -0,0 +1,32 @@
+
+
+public class PruebaGato {
+  public static void main(String[] args) {
+    
+    Gato[] gato = new Gato[4];
+
+    int i;
+
+    System.out.println("Introduzca los datos de los gatos.");
+  
+    for (i = 0; i < 4; i++) {
+      gato[i] = new Gato();
+      System.out.println("Gato num " + (i + 1));
+      System.out.print("Nombre: ");
+      gato[i].setNombre(System.console().readLine());
+      System.out.print("Color: ");
+      gato[i].setColor(System.console().readLine());
+      System.out.print("Raza: ");
+      gato[i].setRaza(System.console().readLine());
+    }
+
+    System.out.println("Aqui tienen los datos de los gatos: ");
+
+    for (i = 0; i < 4; i++) {
+      System.out.println("Gato num" + (i + 1));
+      System.out.println("Nombre: " + gato[i].getNombre());
+      System.out.println("Color: " + gato[i].getColor());
+      System.out.println("Raza: " + gato[i].getRaza());
+    }
+  }
+}
