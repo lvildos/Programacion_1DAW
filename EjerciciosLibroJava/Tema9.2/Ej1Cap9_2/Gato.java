810bcd82d6861e349c6fa8ce936398a1798dae77
lvildos <lvildos588@g.educaand.es>
2023-03-23 21:51:35 +0100

    Tareas

--- EjerciciosLibroJava/Tema9.2/Ej1Cap9_2/Gato.java
+++ EjerciciosLibroJava/Tema9.2/Ej1Cap9_2/Gato.java
@@ -6,31 +6,53 @@
 */
 
 public class Gato {
-  private String nombre;
-  private String color;
-  private String raza;
-  
+  // Aqui estan las variables y los atributos
+  String nombre;
+  int edad;
+  String sexo;
+  static int sardinas = 10;
+  int sardinasComidas = 0;
+
+  // constructor
+  public Gato(String nombre, int edad, String sexo) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.sexo = sexo;
+  }
+
+  // método especifico de las sardinas
+  public void come(int s) {
+    System.out.print(this.nombre + ": ");
+    if (sardinas >= s) {
+      for (int i = 0; i < s; i++) {
+        System.out.print("Ñam...");
+        sardinas--;
+        sardinasComidas++;
+      }
+      System.out.println();
+    } else {
+      System.out.println("Ya no hay sardinas. ");
+    }
+  }
+
+  //métodos getters
   public String getNombre() {
     return nombre;
   }
 
-  public void setNombre(String nombre) {
-    this.nombre = nombre;
-  }
-  
-  public String getColor() {
-    return color;
+  public int getEdad() {
+    return edad;
   }
 
-  public void setColor(String color) {
-    this.color = color;
+  public String getSexo() {
+    return sexo;
   }
 
-  public String getRaza() {
-    return raza;
+  public int getSardinasComidas() {
+    return sardinasComidas;
   }
 
-  public void setRaza(String raza) {
-    this.raza = raza;
+  public static int getSardinas() {
+    return sardinas;
   }
 }
--- EjerciciosLibroJava/Tema9.2/Ej1Cap9_2/PruebaGato.java
+++ EjerciciosLibroJava/Tema9.2/Ej1Cap9_2/PruebaGato.java
@@ -1,32 +1,25 @@
-
+/* PruebaGato.java
+*Utiliza la clase Gato para crear un array de cuatro gatos e introduce los datos
+*de cada uno de ellos mediante un bucle. Muestra a continuación los datos de
+*todos los gatos utilizando también un bucle.
+*@LDS
+*/
 
 public class PruebaGato {
   public static void main(String[] args) {
-    
     Gato[] gato = new Gato[4];
 
-    int i;
-
-    System.out.println("Introduzca los datos de los gatos.");
-  
-    for (i = 0; i < 4; i++) {
-      gato[i] = new Gato();
-      System.out.println("Gato num " + (i + 1));
-      System.out.print("Nombre: ");
-      gato[i].setNombre(System.console().readLine());
-      System.out.print("Color: ");
-      gato[i].setColor(System.console().readLine());
-      System.out.print("Raza: ");
-      gato[i].setRaza(System.console().readLine());
+    // bucle para introducir los datos de cada gato
+    for (int i = 0; i < gato.length; i++) {
+      gato[i] = new Gato("Gato " + (i + 1), i + 1, i % 2 == 0 ? "Macho" : "Hembra");
     }
 
-    System.out.println("Aqui tienen los datos de los gatos: ");
-
-    for (i = 0; i < 4; i++) {
-      System.out.println("Gato num" + (i + 1));
-      System.out.println("Nombre: " + gato[i].getNombre());
-      System.out.println("Color: " + gato[i].getColor());
-      System.out.println("Raza: " + gato[i].getRaza());
+    // Mostrar los datos de todos los gatos
+    for (Gato g : gato) {
+      int sardinas = (int) (Math.random() * 11);
+      g.come(sardinas);
+      System.out.println("Nombre: " + g.getNombre() + ", Edad: " + g.getEdad() + ", Sexo: " + g.getSexo()
+          + ", Sardinas comidas: " + g.getSardinasComidas());
     }
   }
 }
