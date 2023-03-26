a4b4ecc8e734781607b67b504d4d6c60adacecb4
lvildos <lvildos588@g.educaand.es>
2023-03-26 03:12:37 +0200

    Tarea

--- EjerciciosLibroJava/Tema9.1/Ej5Cap9_1/PedidosPizza.java
+++ EjerciciosLibroJava/Tema9.1/Ej5Cap9_1/PedidosPizza.java
@@ -1,15 +1,31 @@
-
+/* PedidosPizza.java
+*Crea la clase Pizza con los atributos y métodos necesarios. Sobre cada pizza
+*se necesita saber el tamaño - mediana o familiar - el tipo - margarita, cuatro
+*quesos o funghi - y su estado - pedida o servida. La clase debe almacenar
+*información sobre el número total de pizzas que se han pedido y que se
+*han servido. Siempre que se crea una pizza nueva, su estado es “pedida”.
+*@LDS
+*/
 
 public class PedidosPizza {
   public static void main(String[] args) {
+
+// Se crean tres instancias  
   Pizza p1 = new Pizza("margarita", "mediana");
   Pizza p2 = new Pizza("funghi", "familiar");
+
+  //Se llama al método sirve
   p2.sirve();
+
   Pizza p3 = new Pizza("cuatro quesos", "mediana");
+
+  //Se imprimen por pantalla las tres pizzas y sus características
   System.out.println(p1);
   System.out.println(p2);
   System.out.println(p3);
   p2.sirve();
+
+  //Se imprime el total de pizzas pedidas y servidas.
   System.out.println("pedidas: " + Pizza.getTotalPedidas());
   System.out.println("servidas: " + Pizza.getTotalServidas());
   }
--- EjerciciosLibroJava/Tema9.1/Ej5Cap9_1/Pizza.java
+++ EjerciciosLibroJava/Tema9.1/Ej5Cap9_1/Pizza.java
@@ -1,12 +1,23 @@
+/* PedidosPizza.java
+*Crea la clase Pizza con los atributos y métodos necesarios. Sobre cada pizza
+*se necesita saber el tamaño - mediana o familiar - el tipo - margarita, cuatro
+*quesos o funghi - y su estado - pedida o servida. La clase debe almacenar
+*información sobre el número total de pizzas que se han pedido y que se
+*han servido. Siempre que se crea una pizza nueva, su estado es “pedida”.
+*@LDS
+*/
 
 
 public class Pizza {
+
+//Aquí se muestran los atributos, incluyendo 2 estáticos.
   private String tipo;
   private String tamanio;
   private String estado;
   private static int totalPedidas = 0;
   private static int totalServidas = 0;
 
+  //Aquí se muestra el constructor
   public Pizza(String tipo, String tamanio) {
       this.tipo = tipo;
       this.tamanio = tamanio;
@@ -14,6 +25,7 @@ public class Pizza {
       totalPedidas++;
   }
 
+  //Aquí se muestran los métodos
   public void sirve() {
       if (estado.equals("pedida")) {
           estado = "servida";
@@ -29,6 +41,7 @@ public class Pizza {
       return totalServidas;
   }
 
+  // El método toString() imprime las características de las pizzas
   @Override
   public String toString() {
       return "Pizza " + tipo + " " + tamanio + ", " + estado;
