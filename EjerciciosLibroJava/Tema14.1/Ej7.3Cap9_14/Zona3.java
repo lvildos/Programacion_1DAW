cac1670b2bbd69e2613da9bfafe643df737225d1
lvildos <lvildos588@g.educaand.es>
2023-04-14 19:06:34 +0200

    Tareas

---
+++ EjerciciosLibroJava/Tema14.1/Ej7.3Cap9_14/Zona3.java
@@ -0,0 +1,38 @@
+/* Zona3.java
+*Aplicación de try-catch
+*@LDS
+*/
+
+public class Zona3 {
+  
+  private int entradasPorVender;
+  
+  public Zona3(int n){
+    entradasPorVender = n;
+  }
+  
+  public int getEntradasPorVender() {
+    return entradasPorVender;
+  }
+  
+  /**
+   * Vende un número de entradas.
+   * Comprueba si quedan entradas libres antes de realizar la venta.
+   * 
+   * @param n número de entradas a vender
+   */
+  public void vender(int n) {
+    
+    if (this.entradasPorVender == 0) {
+      System.out.println("Lo siento, las entradas para esa zona están agotadas.");
+    } else if (this.entradasPorVender < n) {
+      System.out.println("Sólo me quedan " + this.entradasPorVender
+                          + " entradas para esa zona.");
+    }
+    
+    if (this.entradasPorVender >= n) {
+      entradasPorVender -= n;
+      System.out.println("Aquí tiene sus " + n + " entradas, gracias.");
+    }
+  }
+}
